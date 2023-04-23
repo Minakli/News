82437f650bea2dc8fa5d34a76c83495fb88ee135
Minakli <ges-a@ya.ru>
2023-04-23 10:10:30 +0530

    The project is in the process of being created.

--- script.js
+++ script.js
@@ -1,51 +1,100 @@
+const selectCategory = document.querySelector(".select__category");
+
+const newsWrapper = document.querySelector(".news__wrapper");
+const addNews = document.querySelector(".add__news");
+
+const modal = document.querySelector(".modal");
+const modalBtnSend = document.querySelector(".modal__btn_send");
+
+const editor = document.querySelector(".editor");
+const showEditor = document.querySelector(".btn__category_editor");
+const editorUl = document.querySelector(".editor__ul");
+const editorApply = document.querySelector(".editor__apply");
+
+let arrCategory = [];
 let arr = [
   { title: "qwe", body: "qweqweqwe", kategory: 1, id: "id" },
   { title: "asd", body: "asdasdasd", kategory: 2, id: "id" },
   { title: "zxc", body: "zxczcxzxc", kategory: 3, id: "id" },
 ];
-let arrCategory = [
-  {
-    name: "QQQQQQ",
-    id: "Q",
-  },
-  {
-    name: "WWWWWW",
-    id: "W",
-  },
-  {
-    name: "EEEEEE",
-    id: "E",
-  },
-];
-const selectCategory = document.querySelector(".select__category");
-const newsWrapper = document.querySelector(".news__wrapper");
-const addNews = document.querySelector(".add__news");
-const modal = document.querySelector(".modal");
-const modalBtnSend = document.querySelector(".modal__btn_send");
 
-arrCategory.forEach((elem) => {
-  createCategory(elem);
-});
+//Get categories from the server
+async function getArrCategory() {
+  let response = await fetch("http://24api.ru/rest-news-category");
+  arrCategory = await response.json();
+  arrCategory.forEach((elem) => {
+    createCategory(elem);
+  });
+}
+getArrCategory();
 
-arr.forEach((elem) => {
-  createSingleNews(elem);
-});
-
-//Show all news
+// Showing all the news
 function showAllNews() {
   arr.forEach((elem) => {
     createSingleNews(elem);
   });
 }
-//Create categories
+// Creating a category
 function createCategory(obj) {
   const btnCategory = document.createElement("button");
   btnCategory.classList.add("btn__category");
   btnCategory.textContent = obj.name;
   selectCategory.append(btnCategory);
+  btnCategory.addEventListener("click", () => {});
 }
 
-//Create piece of news
+// Deleting a category
+async function deleteCategory(elem) {
+  let response = await fetch("http://24api.ru/rest-news-category/" + elem.id, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+    },
+  });
+  let isDelete = await response.json();
+  console.log(isDelete);
+}
+
+//Editing a category
+async function editCategory(elem) {
+  const text = elem.querySelector(`input[type="text"]`);
+  let unit = { name: "qwe" };
+  unit = arrCategory.find((item) => elem.id == item.id);
+  console.log(unit);
+  if (text.value !== unit.name) {
+    let response = await fetch(
+      "http://24api.ru/rest-news-category/" + unit.id,
+      {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ name: text.value }),
+      }
+    );
+    let isEdited = await response.json();
+    console.log(isEdited);
+  }
+}
+
+// Creating  editor unit
+function createEditorLi(object) {
+  const editorLi = document.createElement("li");
+  editorLi.classList.add("editor__li");
+  editorLi.setAttribute("id", object.id);
+  editorUl.append(editorLi);
+  const editorLiCheckbox = document.createElement("input");
+  editorLiCheckbox.classList.add("editor__checkbox");
+  editorLiCheckbox.setAttribute("type", "checkbox");
+  const editorLiText = document.createElement("input");
+  editorLiText.classList.add("editor__news_text");
+  editorLiText.setAttribute("type", "text");
+  editorLiText.value = object.name;
+  editorLi.append(editorLiCheckbox, editorLiText);
+}
+
+//Creating a piece of news
 function createSingleNews(obj) {
   const singleNews = document.createElement("div");
   singleNews.classList.add("single_news");
@@ -68,6 +117,39 @@ function createSingleNews(obj) {
 modalBtnSend.addEventListener("click", () => {
   modal.style.display = "none";
 });
+
 addNews.addEventListener("click", () => {
   modal.style.display = "block";
+  editor.style.display = "none";
 });
+
+// Displaying the Category Editor
+showEditor.addEventListener("click", () => {
+  modal.style.display = "none";
+  editorUl.innerHTML = "";
+  arrCategory.forEach((elem) => {
+    createEditorLi(elem);
+  });
+  // createEditorLi();
+  editor.style.display = "flex";
+});
+
+editorApply.addEventListener("click", () => {
+  //Get li array
+  let arrLi = editorUl.querySelectorAll("li");
+  console.log(arrLi);
+
+  arrLi.forEach((elem) => {
+    const checkbox = elem.querySelector(`input[type="checkbox"]`);
+    if (checkbox.checked) {
+      deleteCategory(elem);
+    }
+
+    editCategory(elem);
+  });
+
+  arrCategory.forEach(() => {});
+  editor.style.display = "none";
+});
+
+showAllNews();
